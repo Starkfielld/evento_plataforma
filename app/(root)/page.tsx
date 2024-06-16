6e8c4e2d3d448e2b31f9c70daf752170540277b1
Paulo Roberto <paulorobertoneiva2@gmail.com>
2024-06-16 19:20:18 -0300

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
-import CategoryFilter from '@/components/shared/CategoryFilter';
-import Collection from '@/components/shared/Collection'
-import Search from '@/components/shared/Search';
+//import CategoryFilter from '@/components/shared/CategoryFilter';
+//import Collection from '@/components/shared/Collection'
+//import Search from '@/components/shared/Search';
 import { Button } from '@/components/ui/button'
 import { getAllEvents } from '@/lib/actions/event.actions';
 import { SearchParamProps } from '@/types';
@@ -47,19 +47,10 @@ export default async function Home({ searchParams }: SearchParamProps) {
         <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
-          <Search />
-          <CategoryFilter />
+      
         </div>
 
-        <Collection 
-          data={events?.data}
-          emptyTitle="No Events Found"
-          emptyStateSubtext="Come back later"
-          collectionType="All_Events"
-          limit={6}
-          page={page}
-          totalPages={events?.totalPages}
-        />
+       
       </section>
     </>
   )
